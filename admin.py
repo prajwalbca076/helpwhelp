dcd3a968c692cefa8f19d122b0fce6868c55d484
Prajwal <96605299+prajwalbca076@users.noreply.github.com>
2022-06-02 00:35:16 +0545

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-from .models import Whelp
-from .models import Post
-
-admin.site.register(User)
-admin.site.register(Whelp)
-admin.site.register(Post)
